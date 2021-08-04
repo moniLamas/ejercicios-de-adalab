1d83d084013673576ecdc5f260160a4a0449d307
monipeny <mlamasrial@gmail.com>
2021-08-04 19:53:30 +0200

    test let and const in main.js

--- modulo-2/modulo-2-leccion-01-ejercicio-1/main.js
+++ modulo-2/modulo-2-leccion-01-ejercicio-1/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
 document.querySelector('h1').innerHTML = '"Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente"';
+
+
+let myAddress = 'Calle San Pablo';
+myAddress = 'Avenida de Fuenlabrada'
