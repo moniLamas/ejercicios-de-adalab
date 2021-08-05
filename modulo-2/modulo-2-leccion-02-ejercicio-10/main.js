d7020d60b2097f6320df503b68e95114661d82eb
monipeny <mlamasrial@gmail.com>
2021-08-05 20:40:05 +0200

    done exercise 10

---
+++ modulo-2/modulo-2-leccion-02-ejercicio-10/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const age = 24 * 365 * 40;
+
+const myAge = document.querySelector('.myAge');
+
+myAge.innerHTML = `Hola, me llamo Moni y tengo ` + parseInt(age) + ` horas de vida`;
+
+
