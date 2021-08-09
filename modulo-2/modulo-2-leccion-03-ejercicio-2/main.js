4f32aa75b404fa1b1a68c69c046d5f3e23cd7197
monipeny <mlamasrial@gmail.com>
2021-08-09 16:56:57 +0200

    exercise 2 lesson 3

---
+++ modulo-2/modulo-2-leccion-03-ejercicio-2/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const myName = 'Mónica';
+
+let otherName = 'Andrea';
+
+if (otherName === myName) {
+    console.log(`Bienvenida, ${myName}`);
+} 
+else {
+    console.log('Lo siento pero el usuario que has introducido no está registrado')
+}
+
+
