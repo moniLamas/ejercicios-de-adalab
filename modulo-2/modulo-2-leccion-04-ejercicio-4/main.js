ee3070238231209db3541b6f94fedcc1ae64fd43
monipeny <mlamasrial@gmail.com>
2021-08-09 21:01:57 +0200

    exercise 4

---
+++ modulo-2/modulo-2-leccion-04-ejercicio-4/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function parOdd (number) {
+    if (`${number} % 2 === 1`) {
+        console.log(`El número ${number} es par`);
+    } else {
+        console.log(`El número ${number} es impar`)
+    };
+
+    return number;
+}
+
+console.log(9);
+
+
