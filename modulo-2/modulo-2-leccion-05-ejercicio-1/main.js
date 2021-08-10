ad6e48f940ed9860a35716af7d5964c886bcbc78
monipeny <mlamasrial@gmail.com>
2021-08-10 17:53:28 +0200

    modulo 2 leccion 5 exercise 1

---
+++ modulo-2/modulo-2-leccion-05-ejercicio-1/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+debugger;
+
+const button = document.querySelector('.js-btn');
+button.addEventListener('click', () => console.log('Mi primer click, ¡ole yo y la mujer que me parió!'));
+
