3cf4148e601b6bbc648c6d16a763c481b9c1c1c3
monipeny <mlamasrial@gmail.com>
2021-08-10 20:27:07 +0200

    modulo 5 ejercicio 3 text

--- modulo-2/modulo-2-leccion-05-ejercicio-3/main.js
+++ modulo-2/modulo-2-leccion-05-ejercicio-3/main.js
@@ -5,7 +5,7 @@ debugger;
 
 const para = document.querySelector('.js-text');
 
-const newPara = para + para;
+const newPara = `${para} <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem doloribus reprehenderit blanditiis</p>`;
 
 
 para.addEventListener('mouseover', () => p.innerHTML = newPara);
