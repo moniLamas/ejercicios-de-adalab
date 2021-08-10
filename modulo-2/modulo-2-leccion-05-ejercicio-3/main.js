d8e90bee5558975e1c075f36eee0ee1efb1b5555
monipeny <mlamasrial@gmail.com>
2021-08-10 18:54:08 +0200

    test leccion 5 ejercicio 3

---
+++ modulo-2/modulo-2-leccion-05-ejercicio-3/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+debugger;
+
+
+const para = document.querySelector('.js-text');
+
+const newPara = para + para;
+
+
+para.addEventListener('mouseover', () => p.innerHTML = newPara);
+
