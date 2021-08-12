dc80bd1d7e730b4e5c8fb59c46c04731cb32d74b
monipeny <mlamasrial@gmail.com>
2021-08-12 11:11:30 +0200

    modulo 6 ejercicio 7

---
+++ modulo-2/modulo-2-leccion-06-ejercicio-7/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const user = {};
+
+const job = 'developer';
+
+user.name = 'James';
+user.lastName = 'Dops';
+user.age = 23;
+user.job = job;
+
+console.log(user);
+
+user.name = 'Thomas';
+user.age += 1;
+
+console.log(user);
+
+
+
+
