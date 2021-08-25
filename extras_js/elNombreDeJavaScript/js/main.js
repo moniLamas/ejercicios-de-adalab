f28605c01016b4b6f17f77136faeabe41727d1e7
monipeny <mlamasrial@gmail.com>
2021-08-25 17:45:56 +0200

    initial commit extras_js

---
+++ extras_js/elNombreDeJavaScript/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let value = prompt('¿Cuál es el nombre "oficial" de JavaScript?', '');
+
+if (value == 'ECMAScript') {
+    alert('¡CORRECTO!');
+} else {
+    alert('¿No lo sabes? ¡ECMAScript!');
+}
