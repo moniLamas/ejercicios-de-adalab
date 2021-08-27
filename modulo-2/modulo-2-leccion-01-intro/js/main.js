a751cb6f58671b72855fb5f558afafedcf97d2f3
monipeny <mlamasrial@gmail.com>
2021-08-27 15:00:54 +0200

    apply change

--- modulo-2/modulo-2-leccion-01-intro/js/main.js
+++ modulo-2/modulo-2-leccion-01-intro/js/main.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const titleMain = document.querySelector('.js_title');
+// titleMain.innerHTML = "Lección 1: Intro a programación";
+// titleMain.innerHTML = titleMain.innerHTML +": Intro a programación";
+titleMain.innerHTML +=": Intro a programación";
+titleMain.classList.add('red');
