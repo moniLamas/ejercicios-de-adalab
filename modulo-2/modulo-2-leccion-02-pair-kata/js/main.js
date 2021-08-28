04efa4cebb52c85df493de567f90dff91aa7dd55
monipeny <mlamasrial@gmail.com>
2021-08-28 12:44:16 +0200

    apply change main.js

---
+++ modulo-2/modulo-2-leccion-02-pair-kata/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Objetivo:
+//   Debéis poner el texto que está en el span de la derecha del primer div
+// dentro del span del segundo div, transformando el texto en mayúsculas y
+// usando unicamente JavaScript (podéis poner clases en las etiquetas del HTML).
+// Pista: buscad en MDN los métodos de strings.
+//
+// Pasos:
+//
+// ~1. Traer el <span> con el texto original a una constante.~
+// ~2. Traer el <span> donde se escribirá el texto modificado.~
+// ~3. Obtener el texto que contiene.~
+// 4. Transformar el texto a mayúsculas.
+// 5. Poner el texto transformado en el <span> destino.
+
+const origin = document.querySelector('.js_origin');
+const target = document.querySelector('.js_target');
+
+const text = origin.innerHTML;
+const upperText = text.toUpperCase();
+
+target.innerHTML = upperText;
