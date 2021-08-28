a26f81ddf1b8c24cd1ab7d8b3a2798c241193dfc
monipeny <mlamasrial@gmail.com>
2021-08-28 10:42:35 +0200

    apply change to main.js

--- modulo-2/modulo-2-leccion-02-lista-compra/js/main.js
+++ modulo-2/modulo-2-leccion-02-lista-compra/js/main.js
@@ -8,6 +8,8 @@ const item2 = 'Leche';
 const precio2 = 0.65;
 const item3 = 'Tomates';
 const precio3 = 1.35;
+const totalIva = precio1 * 0.21 + precio2 * 0.4 + precio3 * 0.4;
+const total = precio1 + precio2 + precio3 + totalIva;
 
 lista.innerHTML = `<li>${item1} <span>${precio1}</span></li>`;
 // lista.innerHTML = lista.innerHTML + `<li>${item2} <span>${precio2}</span></li>`;
@@ -20,6 +22,9 @@ html += `<li>${item2} <span>${precio2}</span></li>`;
 
 html += `<li>${item3} <span>${precio3}</span></li>`;
 
-html += `<li>TOTAL <span>${precio1 + precio2 + precio3}</span></li>`;
+html += `<li>Total iva <span>${totalIva}</span></li>`;
+
+html += `<li>TOTAL <span>${total}</span></li>`;
+
 
 lista.innerHTML = html;
