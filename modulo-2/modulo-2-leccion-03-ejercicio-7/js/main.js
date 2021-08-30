351c1287fafe56a6c883db482c6eb7bc5548bd40
monipeny <mlamasrial@gmail.com>
2021-08-30 12:44:05 +0200

    leccion3 ejercicio7

---
+++ modulo-2/modulo-2-leccion-03-ejercicio-7/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let ingredient = '';
+
+switch (ingredient) {
+    case 'pollo':
+        console.log('Hoy tomaré filete con patatas');
+        break;
+    case 'merluza':
+        console.log('Hoy tomaré merlucita en salsa verde');
+        break;
+    case 'espinacas':
+        console.log('Hoy tomaré espinacas reogadas');
+        break;
+    default:
+        console.log('Nada en la nevera');
+}
