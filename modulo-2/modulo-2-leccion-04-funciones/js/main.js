cb5723506c5d81824db11bb0c90ad8e342717293
monipeny <mlamasrial@gmail.com>
2021-08-30 15:24:34 +0200

    apply changes main.js leccion4:funciones

---
+++ modulo-2/modulo-2-leccion-04-funciones/js/main.js
@@ -0,0 +1,43 @@
+'use strict';
+const elemSaludo = document.querySelector('.js_saludo');
+
+function saludar(nombre) {
+    elemSaludo.innerHTML += `<p>Hola ${nombre}</p>`;
+}
+
+saludar('Ana');
+saludar('Clara');
+saludar('Daniela');
+saludar('Irene');
+saludar('Paloma');
+
+//Variables
+const inputName = document.querySelector('.js_name');
+const inputLastname = document.querySelector('.js_lastname');
+const elemResultado = document.querySelector('.js_result');
+
+//Funciones
+function cogerNombre() {
+    const name = inputName.value;
+    return name;
+}
+
+function cogerApellido() {
+    return inputLastname.value;
+}
+
+function formarNombreCompleto(nombre, apellido) {
+    const nombreCompleto = `${nombre} ${apellido}`;
+    return nombreCompleto;
+}
+
+function escribirNombreCompleto() {
+    elemResultado.innerHTML = nombreCompleto;
+}
+
+//Operaciones
+
+const nombre = cogerNombre();
+const apellido = cogerApellido();
+const nombreCompleto = formarNombreCompleto(nombre, apellido);
+escribirNombreCompleto(nombreCompleto);
