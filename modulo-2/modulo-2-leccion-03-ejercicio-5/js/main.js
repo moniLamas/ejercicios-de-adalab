96631f0964da4c3e094324e9cf590d0e0d0284f5
monipeny <mlamasrial@gmail.com>
2021-08-30 12:13:55 +0200

    apply change leccion 3 ejercicio 5

--- modulo-2/modulo-2-leccion-03-ejercicio-5/js/main.js
+++ modulo-2/modulo-2-leccion-03-ejercicio-5/js/main.js
@@ -1,17 +1,26 @@
 'use strict';
 
 const message = document.querySelector('.js_message');
+const title = document.querySelector('.js_title');
+const text = document.querySelector('.js_text');
 
 if (message.classList.contains('error')) {
     message.classList.remove('successe');
     message.classList.remove('warning');
-    message.innerHTML = 'ERROR';
+    title.innerHTML = 'ERROR';
+    text.innerHTML = 'Ha surgido un error';
+
 } else if (message.classList.contains('warning')) {
     message.classList.remove('successe');
     message.classList.remove('error');
-    message.innerHTML = 'WARNING';
+    title.innerHTML = 'WARNING';
+    text.innerHTML = 'Tenga cuidado';
 
 } else {
-    message.classList.add('successe');
-    message.innerHTML = 'SUCCESSE';
+    // message.classList.remove('error');
+    // message.classList.remove('warning');
+    // message.classList.add('successe');
+
+    title.innerHTML = 'SUCCESSE';
+    text.innerHTML = 'Los datos son correctos';
 }
