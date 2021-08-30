300b031fd55dbbf5f68b39f9941a3f828cc02e27
monipeny <mlamasrial@gmail.com>
2021-08-30 12:32:33 +0200

    leccion3 ejercicio6

---
+++ modulo-2/modulo-2-leccion-03-ejercicio-6/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let avocados;
+const avocadoPrice = 1.5;
+const money = 45;
+
+// if (money >= avocadoPrice) {
+//     avocados = money / avocadoPrice;
+// } else {
+//     avocados = 0;
+// }
+
+const buy = money >= avocadoPrice ?
+    money / avocadoPrice :
+    avocados = 0;
+
+console.log(buy);
