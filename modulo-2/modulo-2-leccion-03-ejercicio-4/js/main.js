86724c08c09a28cd31f077b631bd657319c91c0a
monipeny <mlamasrial@gmail.com>
2021-08-30 11:39:59 +0200

    apply change leccion 3 ejercicio 4

--- modulo-2/modulo-2-leccion-03-ejercicio-4/js/main.js
+++ modulo-2/modulo-2-leccion-03-ejercicio-4/js/main.js
@@ -1,23 +1,9 @@
 'use strict';
 
-// let numero = 25;
-
-// if (numero === 0) {
-//     console.log('El número es 0')
-// } else if (numero < 0) {
-//     console.log('El número es negativo')
-// } else if ((numero + 2) > 13 && (numero + 2) <= 20) {
-//     console.log('El número más 2 es mayor que 13 pero menor o igual que 20')
-// } else if (numero > 20 && numero < 50) {
-//     console.log('El número es mayor que 20 pero menor que 50')
-// } else {
-//     console.log('el número no es 123123125')
-// }
-
 const dog = 12;
 const humano1 = 15;
 const humano2 = 15 + 9;
-const humano = dog * 5;
+const humano3 = dog * 5;
 
 if (dog === 1) {
     console.log(`Tu perro tiene ${dog} año, equivale a ${humano1} años de humano`);
@@ -25,5 +11,5 @@ if (dog === 1) {
 } else if (dog === 2) {
     console.log(`Tu perro tiene ${dog} años que equivale a ${humano2} años de humano`);
 } else {
-    console.log(`Tu perro tiene ${dog} años que equivale a ${humano} años de humano`);
+    console.log(`Tu perro tiene ${dog} años que equivale a ${humano3} años de humano`);
 }
