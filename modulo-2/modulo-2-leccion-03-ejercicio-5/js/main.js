dae50f07341591252c482b163aea6d2458538e03
monipeny <mlamasrial@gmail.com>
2021-08-30 12:20:08 +0200

    apply change leccion 3 ejercicio 5

--- modulo-2/modulo-2-leccion-03-ejercicio-5/js/main.js
+++ modulo-2/modulo-2-leccion-03-ejercicio-5/js/main.js
@@ -5,22 +5,14 @@ const title = document.querySelector('.js_title');
 const text = document.querySelector('.js_text');
 
 if (message.classList.contains('error')) {
-    message.classList.remove('successe');
-    message.classList.remove('warning');
     title.innerHTML = 'ERROR';
     text.innerHTML = 'Ha surgido un error';
 
 } else if (message.classList.contains('warning')) {
-    message.classList.remove('successe');
-    message.classList.remove('error');
     title.innerHTML = 'WARNING';
     text.innerHTML = 'Tenga cuidado';
 
 } else {
-    // message.classList.remove('error');
-    // message.classList.remove('warning');
-    // message.classList.add('successe');
-
     title.innerHTML = 'SUCCESSE';
     text.innerHTML = 'Los datos son correctos';
 }
