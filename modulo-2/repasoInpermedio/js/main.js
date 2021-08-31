d5f462a60c8c5053bfd6fcf17332b44e633ffd76
monipeny <mlamasrial@gmail.com>
2021-08-31 20:58:58 +0200

    apply change main.js

--- modulo-2/repasoInpermedio/js/main.js
+++ modulo-2/repasoInpermedio/js/main.js
@@ -4,3 +4,23 @@ const container = document.querySelector('.container');
 const faceElement = document.querySelector('.js_face');
 const selectElement = document.querySelector('.js_select');
 const update = document.querySelector('.js_update');
+
+function randomNumber() {
+    let number = Math.round(Math.random() * 100);
+    return number;
+}
+
+
+
+function handlerUpdate() {
+    faceElement.innerHTML = selectElement.value;
+    if (randomNumber() % 2 === 0) {
+        container.classList.remove('backgroundOrange');
+        container.classList.add('backgroundYellow');
+    } else {
+        container.classList.remove('backgroundYellow');
+        container.classList.add('backgroundOrange');
+    }
+}
+
+update.addEventListener('click', handlerUpdate);
