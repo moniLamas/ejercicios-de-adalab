4f378813e0a55ed18d43252df57c943aac1d6ffe
monipeny <mlamasrial@gmail.com>
2021-09-01 19:29:28 +0200

    apply change main.js

--- modulo-2/modulo-2-leccion-06-objetos/js/main.js
+++ modulo-2/modulo-2-leccion-06-objetos/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const table = document.querySelector('.js_table');
+const inputId = document.querySelector('.js_id');
+const inputName = document.querySelector('.js_name');
+constinputBusiness = document.querySelector('.js.business');
+const inputContact = document.querySelector('.js_contact');
+const buttonForm = document.querySelector('.js_button');
+
+
+function hangleClickButtonSave(event) {
+    event.preventDefault();
+}
+
+buttonSave.addEventListener('click', handleClickButtonSave);
