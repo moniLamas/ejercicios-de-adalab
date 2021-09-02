5200de770d54504d324d8bfce04141771701f315
monipeny <mlamasrial@gmail.com>
2021-09-02 10:00:33 +0200

    link js and css into html

--- modulo-2/modulo-2-leccion-09-ejercicio-4/js/main.js
+++ modulo-2/modulo-2-leccion-09-ejercicio-4/js/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const main = document.querySelector('.js-main');
+
+const tasks = [{
+        name: 'Recoger setas en el campo',
+        completed: true
+    },
+    {
+        name: 'Comprar pilas',
+        completed: true
+    },
+    {
+        name: 'Poner una lavadora de blancos',
+        completed: true
+    },
+    {
+        name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
+        completed: false
+    }
+];
+
+for (const data of tasks) {
+    let html = `<li>${data.name}</li>`;
+    main.html += html;
+}
