540e2d41a76f73a78e0b1548b87adaec5a87ad67
monipeny <mlamasrial@gmail.com>
2021-09-02 16:35:49 +0200

    apply change main.js

--- modulo-2/modulo-2-leccion-09-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-09-ejercicio-3/js/main.js
@@ -1,15 +1,25 @@
 'use strict';
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+const parNumbersArray = [];
+const multiplo3Array = [];
+const newArray = [];
 
-function parNumbers() {
-    const parNumbersArray = [];
+function bestLostNumbre() {
     for (let i = 0; i < lostNumbers.length; i++) {
         if (lostNumbers[i] % 2 === 0) {
             parNumbersArray.push(lostNumbers[i]);
+        } else if (lostNumbers[i] % 3 === 0) {
+            multiplo3Array.push(lostNumbers[i]);
         }
     }
-    console.log(parNumbersArray);
+    const concatArray = parNumbersArray.concat(multiplo3Array);
+    newArray.push(concatArray);
+    console.log(newArray);
 }
 
-parNumbers();
+bestLostNumbre();
+
+console.log(parNumbersArray);
+console.log(multiplo3Array);
+console.log(newArray);
