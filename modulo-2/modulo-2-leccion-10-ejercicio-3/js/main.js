3a891db2a0cb4e2b0dc952320bc30b47609aeff5
monipeny <mlamasrial@gmail.com>
2021-09-03 09:04:12 +0200

    apply change in main.js

--- modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
@@ -2,15 +2,20 @@
 
 const userSearch = document.querySelector('.js_user').value;
 
-console.log(userSearch);
 
 const btnSearch = document.querySelector('.js_btnSearch');
 
-function getUserData(event) {
+function getUserData() {
     fetch(`https://api.github.com/users/${userSearch}`)
         .then(response => response.json())
         .then(data => {
-
+            const userName = document.querySelector('.userName');
+            userName = data.login;
+            const img = document.querySelector("img");
+            img.src = data.avatar_url;
+            img.alt = `Foto de ${userName}`;
+            const repo = document.querySelector('.repos');
+            repos = data.public_repos
         })
 }
 
