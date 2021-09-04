e0f1f9a6884a5bf8bcec37990726037fd5b4390b
monipeny <mlamasrial@gmail.com>
2021-09-04 20:44:09 +0200

    fix userName

--- modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
@@ -3,19 +3,21 @@
 const userSearch = document.querySelector('.js_user');
 const btnSearch = document.querySelector('.js_btnSearch');
 
-function getUserData() {
+function getUserData(event) {
+    event.preventDefault();
     let search = userSearch.value;
     console.log('search');
     fetch(`https://api.github.com/users/${search}`)
         .then(response => response.json())
         .then(data => {
             const userName = document.querySelector('.js_userName');
+            console.log(userName);
             userName.innerHTML = data.login;
             const img = document.querySelector("img");
             img.src = data.avatar_url;
             img.alt = `Foto de ${userName}`;
-            const repo = document.querySelector('.js_repos');
-            repo.innerHTML = data.public_repos;
+            const repos = document.querySelector('.js_repos');
+            repos.innerHTML = data.public_repos;
 
         })
 }
