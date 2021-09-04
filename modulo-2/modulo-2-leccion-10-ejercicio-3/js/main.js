688334bf31efa2de496d3e61fc1dfa1511105582
monipeny <mlamasrial@gmail.com>
2021-09-04 21:38:28 +0200

    test avatar_url

--- modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
@@ -10,13 +10,15 @@ function getUserData(event) {
     fetch(`https://api.github.com/users/${search}`)
         .then(response => response.json())
         .then(data => {
+            debugger;
             const userName = document.querySelector('.js_userName');
-            console.log(userName);
+
             userName.innerHTML = data.login;
             const repos = document.querySelector('.js_repos');
             repos.innerHTML = data.public_repos;
+            console.log(data.avatar_url);
 
-            const img = document.querySelector('js_img');
+            const img = document.querySelector('.js_img');
             img.src = data.avatar_url;
             img.alt = `Foto de ${userName}`;
 
