202815402b5802fd53afa436d672405f3a56b0bb
monipeny <mlamasrial@gmail.com>
2021-09-05 11:41:29 +0200

    test ejercicio de clase

--- modulo-2/modulo-2-leccion-10-clase/js/main.js
+++ modulo-2/modulo-2-leccion-10-clase/js/main.js
@@ -2,6 +2,19 @@
 
 const image = document.querySelector('.js_img');
 
+console.log("Antes");
+
+function coverToJs() {
+    debugger;
+    return Response.json();
+}
+
+function renderToHTML() {
+    debugger;
+    console.log(jsonData);
+    image.src = jsonData.url;
+}
+
 fetch('https://random.dog/woof.json')
     .then(coverToJs)
     .then(renderToHTML);
