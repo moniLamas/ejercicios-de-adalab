d821aa388ea51488d8cdb0194e214cb9fda9df44
monipeny <mlamasrial@gmail.com>
2021-09-05 18:49:03 +0200

    apply change main.js

--- modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
@@ -3,20 +3,27 @@
 const orgSearch = document.querySelector('.js_organization');
 const btnSearch = document.querySelector('.js_btnSearch');
 
+let list = [];
+
 function getOrgData(event) {
     event.preventDefault();
     const search = orgSearch.value;
+
 
     fetch(`https://api.github.com/orgs/${search}/repos`)
         .then(response => response.json())
         .then(data => {
             const nameOrg = document.querySelector('.js_nameOrg');
             nameOrg.innerHTML = search;
-            const repos = document.querySelector('.js_repos');
-            repos.innerHTML = data.public_repos;
-            const nameRepo = data.full_name;
+            // const repos = document.querySelector('.js_repos');
+            // repos.innerHTML = data.public_repos;
+            let nameRepo = data.full_name;
+            console.log(nameREpo);
+            list = nameRepo;
             const listRepo = document.querySelector('.js_listRepo');
-            listRepo.innerHTML = `<li>${nameRepo[0]}</li>`;
+            for (let i = 0; i < list.length, i++) {
+                listRepo.innerHTML = `<li>${nameRepo}</li>`;
+            }
         })
 }
 
