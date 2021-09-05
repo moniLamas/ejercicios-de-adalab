aac3395e9af5ac6d2f05b6f0151a2190e497364b
monipeny <mlamasrial@gmail.com>
2021-09-05 10:48:03 +0200

    fix img

--- modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-3/js/main.js
@@ -10,18 +10,22 @@ function getUserData(event) {
     fetch(`https://api.github.com/users/${search}`)
         .then(response => response.json())
         .then(data => {
-            debugger;
             const userName = document.querySelector('.js_userName');
-
             userName.innerHTML = data.login;
             const repos = document.querySelector('.js_repos');
             repos.innerHTML = data.public_repos;
-            console.log(data.avatar_url);
+            // debugger;
+            // console.log(data.avatar_url);
 
-            const img = document.querySelector('.js_img');
+            // const img = document.querySelector('.js_img');
+            let img = document.createElement('img');
+            document.body.append(img);
+            img.style = 'width: 160px';
             img.src = data.avatar_url;
             img.alt = `Foto de ${userName}`;
 
+            // const htmlImg = document.querySelector('.js_img');
+            htmlImg.innerHTML = img;
 
         })
 }
