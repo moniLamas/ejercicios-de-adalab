f39d3135a7675ed8556437b6922d0337b98b03ae
monipeny <mlamasrial@gmail.com>
2021-09-05 20:30:42 +0200

    leccion 2.11 ejercicio 1 NO FINISH

--- modulo-2/modulo-2-leccion-11-ejercicio-2/js/main.js
+++ modulo-2/modulo-2-leccion-11-ejercicio-2/js/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const person = document.querySelector('.js_person');
+const btnSearch = document.querySelector('.js_btnSearch');
+
+let personName;
+
+function getPersonData(event) {
+    event.preventDefault();
+    const search = person.value;
+    console.log(search);
+
+    fetch(`https://swapi.dev/api/people/?search=${search}`)
+        .then(response => response.json())
+        .then(data => {
+            personName = search;
+            const results = data.results;
+            // console.log(search);
+            const namePerson = document.querySelector('.js_name');
+            console.log(results);
+            namePerson.innerHTML = results.name;
+
+            const hairPerson = document.querySelector('js_hairColor');
+            hairPerson.innerHTML = results.hair_color;
+        })
+}
+
+
+btnSearch.addEventListener('click', getPersonData);
