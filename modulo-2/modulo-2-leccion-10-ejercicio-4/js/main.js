bd0fc50b4f9fb970ae7eab3bae0d1a1bfafde8bf
monipeny <mlamasrial@gmail.com>
2021-09-05 16:39:31 +0200

    apply change in main.js

--- modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
@@ -7,15 +7,16 @@ function getOrgData(event) {
     event.preventDefault();
     const search = orgSearch.value;
 
-    fetch(`https://api.github.com/orgs/${search}`)
+    fetch(`https://api.github.com/orgs/${search}/repos`)
         .then(response => response.json())
         .then(data => {
             const nameOrg = document.querySelector('.js_nameOrg');
             nameOrg.innerHTML = search;
             const repos = document.querySelector('.js_repos');
             repos.innerHTML = data.public_repos;
+            const nameRepo = data.full_name;
             const listRepo = document.querySelector('.js_listRepo');
-            listRepo.innerHTML = `<li></li>`;
+            listRepo.innerHTML = `<li>${nameRepo[0]}</li>`;
         })
 }
 
