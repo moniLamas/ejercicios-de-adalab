0aa86ef4d2e8ec53bbd4af34e2d1e7ceee740dcb
monipeny <mlamasrial@gmail.com>
2021-09-05 12:52:04 +0200

    create event listener

--- modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
+++ modulo-2/modulo-2-leccion-10-ejercicio-4/js/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const orgSearch = document.querySelector('.js_organizarion');
+const btnSearch = document.querySelector('.js_btnSearch');
+
+function getOrgData(event) {
+    event.preventDefault();
+    let search = orgSearch.value;
+    console.log(search);
+
+    fetch(`https://api.github.com/orgs/${search}`)
+        .then(response => response.json())
+        .then(data => {
+            const nameOrg = document.querySelector('.js_nameOrg');
+            nameOrg.innerHTML = search;
+            const repos = document.querySelector('.js_repos');
+            repos.innerHTML = data.public_repos;
+            const listRepo = document.querySelector('.js_listRepo');
+            listRepo.innerHTML = `<li></li>`;
+        })
+}
+
+
+
+btnSearch.addEventListener('click', getOrgData);
