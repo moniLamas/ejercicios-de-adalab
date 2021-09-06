a7afc024b9ae6a4cab88b9fc8581a545401c1c7a
monipeny <mlamasrial@gmail.com>
2021-09-06 09:54:19 +0200

    apply change main.js

--- modulo-2/modulo-2-leccion-11-clase/js/main.js
+++ modulo-2/modulo-2-leccion-11-clase/js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const allInput = document.querySelector('.js_input');
+
+function handleKeyUp() {
+    const data = {};
+
+    for (const eachInput of allInput) {
+        const id = eachInput.id;
+        const value = eachInput.value;
+
+        data[id] = value;
+    }
+}
+
+for (const eachInput of allInput) {
+    eachInput.addEventListener('keyup', handleKeyUp);
+}
