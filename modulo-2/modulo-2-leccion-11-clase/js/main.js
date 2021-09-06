c10b25666906dd5fadd096c8c131e60e7a7fc135
monipeny <mlamasrial@gmail.com>
2021-09-06 10:10:26 +0200

    apply change main.js

--- modulo-2/modulo-2-leccion-11-clase/js/main.js
+++ modulo-2/modulo-2-leccion-11-clase/js/main.js
@@ -9,10 +9,24 @@ function handleKeyUp() {
         const id = eachInput.id;
         const value = eachInput.value;
 
-        data[id] = value;
+        data[id] = value; // Se crea el objeto data con los datos introducidos en el formulario. const data = {name: 'Yanelis', lastname: '', address:'', ...}
+
+        localStorage.setItem('data', JSON.stringify(data)); // Guardamos en local los datos del objeto, en formato JSON en texto
     }
 }
 
 for (const eachInput of allInput) {
     eachInput.addEventListener('keyup', handleKeyUp);
+}
+
+//Cuando cargue la página, está en formato texto
+
+const initialDataText = localStorage.getItem('data');
+
+if (initialDataText !== null) {
+    const initialData = JSON.parse(initialDataText); //lo pasa a objeto apto para JS
+
+    for (const id in initialData) {
+        console.log(id);
+    }
 }
