103996e0c90da4def9e2ee8d8a96a3bf96d76d10
monipeny <mlamasrial@gmail.com>
2021-09-06 19:00:49 +0200

    initial commit

---
+++ modulo-2/modulo-2-leccion-12-ejercicio-2/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+// De viaje
+// Crea una página que contenga tres imágenes, usando un select de formulario pregúntale a la usuaria qué ciudad quiere visitar.
+// Si la usuaria elige Madrid, haz que en cada imagen se muestre una foto de Madrid
+// Haz lo mismo para París y Nueva York
+
+//Madrid
+//https://images.freeimages.com/images/large-previews/a8e/madrid-2-1525740.jpg
+//https://images.freeimages.com/images/large-previews/64e/building0-1-1255058.jpg
+//https://images.freeimages.com/images/large-previews/5f1/trafico-nocturno-1442731.jpg
+
+//París
+//https://www.freeimages.com/es/photo/paris-france-1492044
+//https://www.freeimages.com/es/photo/paris-france-9-1520685
+//https://www.freeimages.com/es/photo/louvre-pyramid-1235235
+
+//New York
+//https://www.freeimages.com/es/photo/new-york-city-1451712
+//https://www.freeimages.com/es/photo/new-york-city-1446704
+//https://www.freeimages.com/es/photo/in-a-new-york-minute-1551693
