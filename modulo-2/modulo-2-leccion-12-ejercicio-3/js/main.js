7f989b8d9be6df22523ed7a0a481e073c027cfc6
monipeny <mlamasrial@gmail.com>
2021-09-06 21:57:09 +0200

    initial commit

---
+++ modulo-2/modulo-2-leccion-12-ejercicio-3/js/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const arrayName1 = {
+    name: 'Julia',
+    lastName: 'Ruiz',
+    phone: '888345765'
+}
+
+const arrayName2 = {
+    name: 'Esther',
+    lastName: 'Garcia',
+    phone: '888345765'
+}
+
+const arrayName3 = {
+    name: 'Sonia',
+    lastName: 'Vazquez',
+    phone: '888345765'
+}
