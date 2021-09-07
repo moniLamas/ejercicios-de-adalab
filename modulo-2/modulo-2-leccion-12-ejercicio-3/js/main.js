fcf9fb8d42612a8d6c439b79fa2bd3f4ce3b6ff0
monipeny <mlamasrial@gmail.com>
2021-09-07 07:40:33 +0200

    apply change main.js and index.html

--- modulo-2/modulo-2-leccion-12-ejercicio-3/js/main.js
+++ modulo-2/modulo-2-leccion-12-ejercicio-3/js/main.js
@@ -9,11 +9,27 @@ const arrayName1 = {
 const arrayName2 = {
     name: 'Esther',
     lastName: 'Garcia',
-    phone: '888345765'
+    phone: '357983450'
 }
 
 const arrayName3 = {
     name: 'Sonia',
     lastName: 'Vazquez',
-    phone: '888345765'
+    phone: '135792468'
+}
+
+const yourName = document.querySelector('#name');
+const yourLastName = document.querySelector('.lastName');
+const yourPhone = document.querySelector('.phone');
+
+if (yourName === arrayName1.name) {
+    yourLastName.value = arrayName1.lastName;
+    yourPhone.value = arrayName1.phone;
+    console.log(yourName);
+} else if (yourName === arrayName2.name) {
+    yourLastName.value = arrayName2.lastName;
+    yourPhone.value = arrayName2.phone
+} else if (yourName === arrayName3.name) {
+    yourLastName.value = arrayName3.lastName;
+    yourPhone.value = arrayName3.phone
 }
