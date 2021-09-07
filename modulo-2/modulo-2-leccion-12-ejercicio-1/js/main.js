86ff1e7fb248cea8c4143f168f3251b482a7d032
monipeny <mlamasrial@gmail.com>
2021-09-07 09:53:30 +0200

    last commit

--- modulo-2/modulo-2-leccion-12-ejercicio-1/js/main.js
+++ modulo-2/modulo-2-leccion-12-ejercicio-1/js/main.js
@@ -1,17 +1,14 @@
 'use strict';
 
+const items = document.querySelector('.items');
+
 const numbers = [1, 2, 3];
 
 for (let eachNumber of numbers) {
     const newItem = document.createElement('li');
     const newContent = document.createTextNode(eachNumber);
-    console.log(eachNumber);
 
     newItem.appendChild(newContent);
-    console.log(newContent);
 
-    const items = document.querySelector('.items');
-    console.log(items);
     items.appendChild(newItem);
-    console.log(newItem);
 }
