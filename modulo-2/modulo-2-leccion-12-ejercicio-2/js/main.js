47da3384826600d5a33a5ed43d7fba7bfe3acb6c
monipeny <mlamasrial@gmail.com>
2021-09-07 17:39:04 +0200

    leccion 12 ejercicio 3 finish

--- modulo-2/modulo-2-leccion-12-ejercicio-2/js/main.js
+++ modulo-2/modulo-2-leccion-12-ejercicio-2/js/main.js
@@ -4,33 +4,90 @@
 // Si la usuaria elige Madrid, haz que en cada imagen se muestre una foto de Madrid
 // Haz lo mismo para París y Nueva York
 
+const city1 = [{
+        name: 'Madrid',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Palaciorealycatedraldelaalmudena_%28cropped%29.jpg/280px-Palaciorealycatedraldelaalmudena_%28cropped%29.jpg',
+        alt: 'Imagen del Palacio Real'
+    },
+    {
+        name: 'Madrid',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Torres_de_Madrid_%28detalle%29.JPG',
+        alt: 'Imagen de las Torres Madrid'
+    },
+    {
+        name: 'Madrid',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg/2560px-Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg',
+        alt: 'Imagen de La Gran Vía'
+    },
+];
 
-//Madrid
-//https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Palaciorealycatedraldelaalmudena_%28cropped%29.jpg/2880px-Palaciorealycatedraldelaalmudena_%28cropped%29.jpg
-//https://upload.wikimedia.org/wikipedia/commons/1/1c/Torres_de_Madrid_%28detalle%29.JPG
-//https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg/2560px-Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg
+const city2 = [{
+        name: 'París',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/2560px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg',
+        alt: 'Imagen de la Torre Eiffel'
+    },
+    {
+        name: 'París',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Arcdetriomphe_2.jpg/2560px-Arcdetriomphe_2.jpg',
+        alt: 'Imagen del Arco del Triunfo'
+    },
+    {
+        name: 'París',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Louvre_Courtyard%2C_Looking_West.jpg/2880px-Louvre_Courtyard%2C_Looking_West.jpg',
+        alt: 'Imagen del Louvre'
+    }
+]
 
-//Paris
-//https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg/2560px-La_Tour_Eiffel_vue_de_la_Tour_Saint-Jacques%2C_Paris_ao%C3%BBt_2014_%282%29.jpg
-//https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Arcdetriomphe_2.jpg/2560px-Arcdetriomphe_2.jpg
-//https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Avenue_des_Champs-%C3%89lys%C3%A9es_July_24%2C_2009_N1.jpg/440px-Avenue_des_Champs-%C3%89lys%C3%A9es_July_24%2C_2009_N1.jpg
-
-//New York
-//https://commons.wikimedia.org/wiki/File:New_york_times_square-terabass.jpg
-//https://commons.wikimedia.org/wiki/File:Lower_Manhattan_skyline_-_June_2017.jpg
-//https://commons.wikimedia.org/wiki/File:Manhattan_at_Dusk_by_slonecker.jpg
+const city3 = [{
+        name: 'New York',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Lower_Manhattan_skyline_-_June_2017.jpg/2880px-Lower_Manhattan_skyline_-_June_2017.jpg',
+        alt: 'Imagen de lower Manhattan'
+    }, {
+        name: 'New York',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/New_york_times_square-terabass.jpg/2560px-New_york_times_square-terabass.jpg',
+        alt: 'Imagen de Times Square',
+    },
+    {
+        name: 'New York',
+        src: 'https://upload.wikimedia.org/wikipedia/commons/0/0b/Qwertyliberty123.jpg',
+        alt: 'Imagen de la estatua de la Libertad'
+    }
+]
 
 const select = document.querySelector('#city');
 const image1 = document.querySelector('.pic1');
 const image2 = document.querySelector('.pic2');
 const image3 = document.querySelector('.pic3');
 
-if (select.value === 'Madrid') {
-    image1.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Palaciorealycatedraldelaalmudena_%28cropped%29.jpg/280px-Palaciorealycatedraldelaalmudena_%28cropped%29.jpg';
-    image1.alt = 'Imagen de Madrid';
-    image2.src = 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Torres_de_Madrid_%28detalle%29.JPG';
-    image2.alt = 'Imagen de Madrid';
-    image3.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg/2560px-Inicio_de_la_Gran_V%C3%ADa_%28cropped%29.jpg';
-    image3.alt = 'Imagen de Madrid';
+function handleOption(event) {
+    event.preventDefault();
 
+    if (select.value === city1[0].name) {
+        image1.src = city1[0].src;
+        image1.alt = city1[0].alt;
+        image2.src = city1[1].src;
+        image2.alt = city1[1].alt;
+        image3.src = city1[2].src;
+        image3.alt = city1[2].alt;
+
+    } else if (select.value === city2[0].name) {
+        image1.src = city2[0].src;
+        image1.alt = city2[0].alt;
+        image2.src = city2[1].src;
+        image2.alt = city2[1].alt;
+        image3.src = city2[2].src;
+        image3.alt = city2[2].alt;
+
+    } else if (select.value === city3[0].name) {
+        image1.src = city3[0].src;
+        image1.alt = city3[0].alt;
+        image2.src = city3[1].src;
+        image2.alt = city3[1].alt;
+        image3.src = city3[2].src;
+        image3.alt = city3[2].alt;
+
+    }
 }
+
+
+select.addEventListener('change', handleOption);
