288f8d1a06c19eee4204147b1198363006856762
monipeny <mlamasrial@gmail.com>
2021-09-07 18:37:54 +0200

    depurate lección 2.13 ejercicio 2

--- modulo-2/modulo-2-leccion-13-ejercicio-2/js/main.js
+++ modulo-2/modulo-2-leccion-13-ejercicio-2/js/main.js
@@ -1,13 +1,5 @@
 'use strict';
 
-// const marks = [5, 4, 6, 7, 9];
-
-// const inflatedMarks = marks.map((mark) => mark + 1);
-
-// console.log(inflatedMarks);
-
-// Bienvenida Yolanda
-
 const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
 
 const greeting = names.map((greet) => 'Bienvenida ' + greet);
