54fa149ec317e89f5e0b0e9fb25712d9f3ade4f2
monipeny <mlamasrial@gmail.com>
2021-09-07 18:37:14 +0200

    lección 2.13 ejercicio 2

---
+++ modulo-2/modulo-2-leccion-13-ejercicio-2/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const marks = [5, 4, 6, 7, 9];
+
+// const inflatedMarks = marks.map((mark) => mark + 1);
+
+// console.log(inflatedMarks);
+
+// Bienvenida Yolanda
+
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+
+const greeting = names.map((greet) => 'Bienvenida ' + greet);
+
+console.log(greeting);
