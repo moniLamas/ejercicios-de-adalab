e537953945acd6dab33fb7a76be5684eb441929a
monipeny <mlamasrial@gmail.com>
2021-09-17 07:32:31 +0200

    add html

--- modulo-3/leccion-01/ejercicio-1.5-1/src/components/App.js
+++ modulo-3/leccion-01/ejercicio-1.5-1/src/components/App.js
@@ -1,10 +1,39 @@
 import '../styles/App.css';
 
 function App() {
-  return ( < div >
+  <
+  > < header className = "header" >
     <
-    h1 > Hola Moni! < /h1>   </div >
-  );
+    div className = "logo" >
+    <
+    a href = "https://adalab.es"
+  target = "_blanck"
+  title = "logo-de-adalab" >
+    <
+    img
+  src = {
+    logo
+  }
+  alt = "logo-adalab"
+  title = "logo-de-adalab" / > < /a> < /
+    div > <
+    nav className = "menu" >
+    <
+    ul className = "ul-menu" >
+    <
+    li > < a href = "./blog.html"
+  className = "link"
+  target = "_blanck" > Blog < /a></li >
+    <
+    li > < a href = "#"
+  className = "link" > Contacto < /a></li >
+    <
+    /ul> < /
+    nav > <
+    /header><main className="main"> <
+  h1 className = "h1" > Creando diversidad digital < /h1>  <
+    /main></ >
+);
 }
 
 export default App;
