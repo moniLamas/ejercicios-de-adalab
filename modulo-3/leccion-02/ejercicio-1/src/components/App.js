aab954f06bac2920359de0bf07adf018e0cd9d49
monipeny <mlamasrial@gmail.com>
2021-09-19 18:48:50 +0200

    apply change code function App

--- modulo-3/leccion-02/ejercicio-1/src/components/App.js
+++ modulo-3/leccion-02/ejercicio-1/src/components/App.js
@@ -1,11 +1,17 @@
 import "../styles/App.scss";
 
+// Fichero src/components/App.js
 function App() {
+  const handleInput = (ev) => {
+    console.log("El evento lanzado es: ", ev);
+    console.log("La tecla pulsada es: ", ev.key);
+    console.log("El valor del input es: ", ev.target.value);
+  };
+
   return (
     <div>
-      <h1> Hola Moni!</h1>
+      <input type="text" name="email" id="email" onKeyUp={handleInput} />
     </div>
   );
 }
-
 export default App;
