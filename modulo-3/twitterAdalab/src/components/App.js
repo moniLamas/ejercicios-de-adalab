140b0f82091cad23a247d560084dde024d2efcdc
monipeny <mlamasrial@gmail.com>
2021-09-20 17:50:56 +0200

    change package.json

--- modulo-3/twitterAdalab/src/components/App.js
+++ modulo-3/twitterAdalab/src/components/App.js
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "../styles/App.scss";
 
 function App() {
+  const [number, setNumber] = useState(0);
+  function increase() {
+    return setNumber(number + 1);
+  }
+  function decrease() {
+    return setNumber(number - 1);
+  }
+  function reset() {
+    setNumber(0);
+  }
   return (
     <div>
-      <h1> Hola Moni!</h1>
+      <h1>El Contador</h1>
+      <p>Contador: {number}</p>
+      <button onClick={increase}>Incrementa 1</button>
+      <button onClick={decrease}>Decrementa 1</button>
+      <button onClick={reset}>Resetea</button>
     </div>
   );
 }
