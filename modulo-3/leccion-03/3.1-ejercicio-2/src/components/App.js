01882a2e67bc28c8e99e159ee01df02a2bf685b5
monipeny <mlamasrial@gmail.com>
2021-09-22 12:59:31 +0200

    calculadora non-functional

--- modulo-3/leccion-03/3.1-ejercicio-2/src/components/App.js
+++ modulo-3/leccion-03/3.1-ejercicio-2/src/components/App.js
@@ -2,10 +2,46 @@ import { useState } from "react";
 
 const App = () => {
   const [numberA, setNumberA] = useState(0);
+  const [numberB, setNumberB] = useState(0);
 
-  const handleNumberA = (ev) => {};
+  const handleNumberA = (ev) => {
+    const newValue = parseInt(ev.currentTarget.value);
+    setNumberA(newValue);
+  };
+  const handleNumberB = (ev) => {
+    const newValue = parseInt(ev.currentTarget.value);
+    setNumberB(newValue);
+  };
 
-  return <div></div>;
+  return (
+    <div>
+      <h1>La calculadora:</h1>
+      <form>
+        <label>
+          Escribe un número:
+          <input
+            type="number"
+            name="name"
+            onChange={handleNumberA}
+            value={numberA}
+          />
+        </label>
+        <label>
+          Escribe otro número:
+          <input
+            type="number"
+            name="email"
+            onChange={handleNumberB}
+            value={numberB}
+          />
+        </label>
+      </form>
+      <p>
+        La suma de <strong>{numberA}</strong> y <strong>{numberB}</strong> es
+        <strong> {total}</strong>.
+      </p>
+    </div>
+  );
 };
 
 export default App;
