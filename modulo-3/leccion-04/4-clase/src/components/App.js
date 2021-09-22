5eb4ae8428be2170a70e6fba8c932815f2779db6
monipeny <mlamasrial@gmail.com>
2021-09-22 10:18:04 +0200

    done

--- modulo-3/leccion-04/4-clase/src/components/App.js
+++ modulo-3/leccion-04/4-clase/src/components/App.js
@@ -1,10 +1,15 @@
 // Fichero src/components/App.js
 import { useState } from "react";
 // import data from './data.json';
-
+// import styles
 const App = () => {
   // Estados
   const [persons, setPersons] = useState(data);
+  const [searchName, setSearch] = useState("");
+
+  const handleSearchName = (ev) => {
+    setSearch(ev.target.value);
+  };
 
   const handleClick = (ev) => {
     const id = ev.currentTarget.id;
@@ -14,23 +19,30 @@ const App = () => {
   };
 
   const renderPerson = () => {
-    return persons.map((person, index) => {
-      return (
-        <li
-          key={index}
-          onClick={handleClick}
-          id={index}
-          className={`classLi ${person.isBlogger ? "background" : ""}`}
-        >
-          <img src="{person.medim}" alt="{person.name}" />
-          <p>{person.name + "" + person.last}</p>
-        </li>
-      );
-    });
+    return persons
+      .filter((person) => {
+        return person.name.toLowerCase().includes(searchName.toLowerCase());
+      })
+      .map((person, index) => {
+        return (
+          <li
+            key={index}
+            onClick={handleClick}
+            id={index}
+            className={`classLi ${person.isBlogger ? "background" : ""}`}
+          >
+            <img src="{person.medim}" alt="{person.name}" />
+            <p>{person.name + "" + person.last}</p>
+          </li>
+        );
+      });
   };
 
   return (
     <div>
+      <form action="">
+        <input type="text" id="searchName" onChange={handleSearchName} />
+      </form>
       <ul>{renderPerson()}</ul>
     </div>
   );
