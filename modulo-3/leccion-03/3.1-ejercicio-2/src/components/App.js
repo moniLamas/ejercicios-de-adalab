0c6ca63e2da99bdc4a8214f11a9d951c66f2042f
monipeny <mlamasrial@gmail.com>
2021-09-22 12:48:16 +0200

    calculadora

---
+++ modulo-3/leccion-03/3.1-ejercicio-2/src/components/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+const App = () => {
+  const [numberA, setNumberA] = useState(0);
+
+  const handleNumberA = (ev) => {};
+
+  return <div></div>;
+};
+
+export default App;
