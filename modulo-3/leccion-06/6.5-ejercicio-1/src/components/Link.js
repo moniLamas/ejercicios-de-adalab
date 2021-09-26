93a7a2fca36d02cb4e679a9928496a4861bbe047
monipeny <mlamasrial@gmail.com>
2021-09-26 19:33:06 +0200

    create new components styles

---
+++ modulo-3/leccion-06/6.5-ejercicio-1/src/components/Link.js
@@ -0,0 +1,20 @@
+import "../styles/App.scss";
+
+function Link(props) {
+  const openInNewTab = props.target === undefined ? "" : props.target;
+  return (
+    <li>
+      <a
+        title={props.title}
+        className={props.class}
+        href={props.href}
+        target={openInNewTab}
+        rel={props.rel}
+      >
+        {props.text}
+      </a>
+    </li>
+  );
+}
+
+export default Link;
