d58c0f85e12855a6a60ce9fd2a250aebec0d8385
monipeny <mlamasrial@gmail.com>
2021-10-12 17:21:27 +0200

    get y post server

--- modulo-4/modulo-4-leccion-02-express-js/basic-server/web_for_testing/src/components/App.js
+++ modulo-4/modulo-4-leccion-02-express-js/basic-server/web_for_testing/src/components/App.js
@@ -1,15 +1,21 @@
-import '../stylesheets/App.scss';
+import "../stylesheets/App.scss";
 
 function App() {
+  const handleClick = () => {
+    fetch("http://localhost:3000/users", { method: "GET" })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <div className="App">
       <header>
-        <h1>
-          Probando nuestro servidor
-        </h1>
+        <h1>Probando nuestro servidor</h1>
       </header>
       <main>
-    
+        <button onClick={handleClick}>Fetch me</button>
       </main>
     </div>
   );
