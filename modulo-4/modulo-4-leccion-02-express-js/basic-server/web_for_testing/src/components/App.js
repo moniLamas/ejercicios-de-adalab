37329d61b6d3ee1ce64ce732e7edd26342f4769d
monipeny <mlamasrial@gmail.com>
2021-10-12 14:40:03 +0200

    delete node modules

---
+++ modulo-4/modulo-4-leccion-02-express-js/basic-server/web_for_testing/src/components/App.js
@@ -0,0 +1,18 @@
+import '../stylesheets/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>
+          Probando nuestro servidor
+        </h1>
+      </header>
+      <main>
+    
+      </main>
+    </div>
+  );
+}
+
+export default App;
